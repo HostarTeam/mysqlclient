1ee97d85e8a4f1b4502897c6afb57b6f806c30b1
Uri Bar Ilan <ubarilan@gmail.com>
2022-02-23 18:05:29 +0200

    Added safety checks and some utility functions

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import {
 import { QueryRow, valueParam } from './typing/types';
 
 export class MySQLClient {
-    private connection!: Connection;
+    private connection: Connection | null | undefined;
 
     constructor(private config: ConnectionOptions) {
         this.config = config;
@@ -25,15 +25,68 @@ export class MySQLClient {
         });
     }
 
+    public isConnected(): Promise<boolean> {
+        if (!this.connection) return new Promise((resolve) => resolve(false));
+        return new Promise((resolve) => {
+            this.connection!.ping((err) => {
+                if (err) return resolve(false);
+                return resolve(true);
+            });
+        });
+    }
+
+    public getConnectionCallback(
+        callback: (connection: Connection | null | undefined) => void
+    ) {
+        if (!this.connection) return callback(null);
+        this.connection!.ping((err) => {
+            if (err) return callback(null);
+            return callback(this.connection);
+        });
+    }
+
+    public getConnection(): Promise<Connection | null | undefined> {
+        return new Promise((resolve) => {
+            this.getConnectionCallback((connection) => {
+                if (!connection) return resolve(null);
+                return resolve(connection);
+            });
+        });
+    }
+
+    public isConnectedWithCallback(callback: (connected: boolean) => void) {
+        this.getConnectionCallback((connection) => {
+            return callback(!!connection);
+        });
+    }
+
+    public isConnectedAsync(): Promise<boolean> {
+        return new Promise((resolve) => {
+            this.isConnectedWithCallback((connected) => {
+                return resolve(connected);
+            });
+        });
+    }
+
     public getQueryResult(
         sql: string,
         values: valueParam = []
     ): Promise<QueryRow[]> {
+        if (!this.connection) throw new Error('Not connected to database');
         return new Promise((resolve, reject) => {
-            this.connection.query(sql, values, (err, rows: RowDataPacket[]) => {
-                if (err) return reject(err);
-                else return resolve(rows);
+            this.getConnectionCallback((connection) => {
+                if (!connection)
+                    return reject(new Error('Not connected to database'));
             });
+
+            this.connection!.query(
+                sql,
+                values,
+                (err, rows: RowDataPacket[]) => {
+                    if (err) return reject(err);
+                    else return resolve(rows);
+                }
+            );
         });
     }
 
